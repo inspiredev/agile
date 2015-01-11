af06fc8733c72cc76b1b0c812a8b50bb3c294949
Tri Nguyen <tri@tridnguyen.com>
2015-01-10 19:34:17 -0500

    add gulp js, jscs and jshint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
+	browserify = require('browserify'),
+	jscs = require('gulp-jscs'),
+	jshint = require('gulp-jshint'),
 	plumber = require('gulp-plumber'),
 	prefix = require('gulp-prefix'),
 	csso = require('gulp-csso'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	watchify = require('watchify'),
+	xtend = require('xtend');
 
 gulp.task('scss', function () {
 	return gulp.src('./scss/**/*.scss')
@@ -22,3 +27,48 @@ gulp.task('scss', function () {
 		.pipe(gulp.dest('./'));
 });
 
+gulp.task('jshint', function () {
+	return gulp.src(['**/*.js', '!node_modules/*'])
+		.pipe(jshint())
+		.pipe(jshint.reporter(require('jshint-stylish')));
+});
+
+gulp.task('jscs', function () {
+	return gulp.src(['**/*.js', '!node_modules/*'])
+		.pipe(jscs());
+});
+
+var watching = false;
+gulp.task('enable-watch-mode', function () { watching = true });
+
+gulp.task('scripts', function () {
+	var opts = {
+		entries: './js/main.js',
+		debug: (gutil.env.type === 'development')
+	}
+	if (watching) {
+		opts = xtend(opts, watchify.args);
+	}
+	var bundler = browserify(opts);
+	if (watching) {
+		bundler = watchify(bundler);
+	}
+	// optionally transform
+	// bundler.transform('transformer');
+
+	bundler.on('update', function (ids) {
+		gutil.log('File(s) changed: ' + gutil.colors.cyan(ids));
+		gutil.log('Rebunlding...');
+	});
+
+	function rebundle() {
+		return bundler
+			.bundle()
+			.on('error', function (e) {
+				gutil.log(gutil.colors.red('Browserify ' + e));
+			})
+			.pipe(source('main.js'))
+			.pipe(gulp.dest(path.js.dest));
+	}
+	return rebundle();
+});
