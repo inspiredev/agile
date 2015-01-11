c2b890edf911a50ad7038a61db7b687c49e77183
Tri Nguyen <tri@tridnguyen.com>
2015-01-10 19:38:55 -0500

    enqueue main script

--- functions.php
+++ functions.php
@@ -106,6 +106,8 @@ function agile_scripts() {
 
 	wp_enqueue_script( 'agile-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	wp_enqueue_script( 'agile-main-js', get_template_directory_uri() . '/js/dist/main.js', array(), false, true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
