6e775f799692035b97479db3ee816414efa25db0
Tri Nguyen <tri@tridnguyen.com>
2015-01-21 22:05:47 -0500

    background colors

--- js/main.js
+++ js/main.js
@@ -9,11 +9,14 @@ jQuery(document).ready(function ($) {
 	$('.top-level .category-name').on('click', function () {
 		var $parentLi = $(this).closest('li');
 		$('.main-navigation').toggleClass('item-active');
-		$('.top-level > li').not($parentLi).removeClass('active').toggleClass('inactive');
+		$('.top-level > li').not($parentLi).removeClass('active').toggleClass('inactive')
+			.find('.category-name').removeClass('active');
 		$parentLi.toggleClass('active');
+		$(this).toggleClass('active');
 	});
 	$('.back').on('click', function () {
 		$('.main-navigation').removeClass('item-active');
 		$('.top-level > li').removeClass('active').removeClass('inactive');
+		$('.category-name').removeClass('active');
 	})
 });
