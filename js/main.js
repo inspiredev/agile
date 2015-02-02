7ee6d497965894dd94a946e106232038eb6fceef
Tri Nguyen <tri@tridnguyen.com>
2015-02-02 17:12:31 -0500

    tab hash

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ jQuery(document).ready(function ($) {
 		$('#page').toggleClass('menu-active');
 	});
 
-	$('.top-level .category-name').on('click', function () {
+	$('.top-level .category-name').on('click', function (e) {
+		e.preventDefault();
 		var $parentLi = $(this).closest('li');
 		$('.main-navigation').toggleClass('item-active');
 		$('.top-level > li').not($parentLi).removeClass('active').addClass('inactive')
@@ -16,9 +17,23 @@ jQuery(document).ready(function ($) {
 		$parentLi.toggleClass('inactive active');
 		$(this).toggleClass('active');
 	});
+
 	$('.back').on('click', function () {
 		$('.main-navigation').removeClass('item-active');
 		$('.top-level > li').removeClass('active').removeClass('inactive');
 		$('.category-name').removeClass('active');
-	})
+	});
+
+	// Change hash for page-reload
+	$('.release-tabs .tab').on('show.bs.tab', function (e) {
+		if (history.pushState) {
+			history.pushState(null, null, this.href);
+		} else {
+			window.location.hash = this.hash;
+		}
+	});
+	// enable link on tab
+	if (location.hash !== '') {
+		$('.release-tabs .tab[href=' + location.hash + ']').tab('show') ;
+	}
 });
